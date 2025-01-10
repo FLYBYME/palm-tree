7e7ac34c1fa05f0cb001db551f6b8a611cc8dc3b
FLYBYME <mangoraft@gmail.com>
2025-01-09 23:57:14 -0600

    Refactor reboot command to use terminal.exec and add listProcesses action for node management

--- mixins/node.actions.mixin.js
+++ mixins/node.actions.mixin.js
@@ -24,13 +24,37 @@ module.exports = {
                     );
                 }
 
-                return ctx.call('v1.ssh.exec', { node: id, command: 'sudo reboot' })
+                return ctx.call('v1.terminal.exec', { node: id, command: 'sudo reboot' })
                     .then(() => {
                         return this.updateEntity(ctx, {
                             id: node.id,
                             status: 'rebooting'
                         });
                     });
+            }
+        },
+        listProcesses: {
+            rest: {
+                method: "GET",
+                path: "/:id/processes",
+            },
+            params: {
+                id: { type: "string", optional: false }
+            },
+            async handler(ctx) {
+                const { id } = ctx.params;
+
+                const node = await this.getNodeById(ctx, id);
+                if (!node) {
+                    throw new MoleculerClientError(
+                        `Node with id ${id} not found`,
+                        404,
+                        "NODE_NOT_FOUND",
+                        { id }
+                    );
+                }
+
+                return ctx.call('v1.terminal.exec', { node: id, command: 'ps aux' });
             }
         },
     }
